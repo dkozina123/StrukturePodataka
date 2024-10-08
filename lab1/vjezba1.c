a0cd932a1f5727a7b77890390beb5a6493ffca50
mbozin01 <mbozin01@fesb.hr>
2024-10-08 20:28:08 +0200

    nedovrseno

--- lab1/vjezba1.c
+++ lab1/vjezba1.c
@@ -1,7 +1,58 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#define ERROR_OPENING_FILE -1
 #include<stdio.h>
+#define BUFFER_SIZE 1024
+
+typedef struct {
+	char ime[10];
+	char prezime[10];
+	int bod;
+}Student;
+
+int count_rows(char* filename);
+
+
 
 int main()
 {
-	printf("hello world\n");
+	FILE* f=NULL;
+
+	f = fopen("studenti.txt", "r");
+	
+	
+
+	
+	
+	fclose(f);
+	
 	return 0;
 }
+
+
+int count_rows(char* filename); {
+	
+	FILE* fp = NULL;
+	char buffer[BUFFER_SIZE];
+	int count == 0;
+
+	fp = fopen(filename, "r");
+	if (f == NULL)
+	{
+		printf("neuspjesno otvaranje");
+		return ERROR_OPENING_FILE;
+	}
+	while (!feof(f)) {
+		fgets(buffer, BUFFER_SIZE, fp);
+		count++;
+	}
+
+	fclose(fp);
+
+	return count;
+}
+
+
+
+
+
